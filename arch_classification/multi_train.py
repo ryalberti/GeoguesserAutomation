6a3ab544926db2fe5e067630895dcf924565573b
ryalberti <ealberti@stevens.edu>
2023-12-11 15:18:36 -0500

    update multi_train

--- arch_classification/multi_train.py
+++ arch_classification/multi_train.py
@@ -2,13 +2,13 @@
 # https://vijayabhaskar96.medium.com/multi-label-image-classification-tutorial-with-keras-imagedatagenerator-cd541f8eaf24 
 
 # imports 
-from keras.models import Sequential
 from keras_preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
 from keras import regularizers, optimizers
 import pandas as pd
 import numpy as np
+import os 
 
 # set directories 
 dir_path = os.getcwd() # get current directory , \GeoguesserAutomation\arch_classification
@@ -67,42 +67,50 @@ test_generator=test_datagen.flow_from_dataframe(
     target_size=(100,100))
 
 
-# I think here and below is what I need to change. 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=(100,100,3)))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(5, activation='sigmoid'))
-model.compile(optimizers.rmsprop(lr=0.0001, decay=1e-6),loss="binary_crossentropy",metrics=["accuracy"]) 
+inp = Input(shape = (100,100,3))
+x = Conv2D(32, (3, 3), padding = 'same')(inp)
+x = Activation('relu')(x)
+x = Conv2D(32, (3, 3))(x)
+x = Activation('relu')(x)
+x = MaxPooling2D(pool_size = (2, 2))(x)
+x = Dropout(0.25)(x)
+x = Conv2D(64, (3, 3), padding = 'same')(x)
+x = Activation('relu')(x)
+x = Conv2D(64, (3, 3))(x)
+x = Activation('relu')(x)
+x = MaxPooling2D(pool_size = (2, 2))(x)
+x = Dropout(0.25)(x)
+x = Flatten()(x)
+x = Dense(512)(x)
+x = Activation('relu')(x)
+x = Dropout(0.5)(x)
+output1 = Dense(1, activation = 'sigmoid')(x)
+output2 = Dense(1, activation = 'sigmoid')(x)
+output3 = Dense(1, activation = 'sigmoid')(x)
+output4 = Dense(1, activation = 'sigmoid')(x)
+output5 = Dense(1, activation = 'sigmoid')(x)
+model = Model(inp,[output1,output2,output3,output4,output5])
+model.compile(optimizers.rmsprop(lr = 0.0001, decay = 1e-6),
+loss = ["binary_crossentropy","binary_crossentropy","binary_crossentropy","binary_crossentropy","binary_crossentropy"],metrics = ["accuracy"])
+
+def generator_wrapper(generator):
+    for batch_x,batch_y in generator:
+        yield (batch_x,[batch_y[:,i] for i in range(5)])
 
 STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
 STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
 STEP_SIZE_TEST=test_generator.n//test_generator.batch_size
-model.fit_generator(generator=train_generator,
-                    steps_per_epoch=STEP_SIZE_TRAIN,
-                    validation_data=valid_generator,
-                    validation_steps=STEP_SIZE_VALID,
-                    epochs=10
-)
+model.fit_generator(generator=generator_wrapper(train_generator),
+                    steps_per_epoch=STEP_SIZE_TRAIN)
+
+validation_data=generator_wrapper(valid_generator,
+                                validation_steps=STEP_SIZE_VALID,
+                                epochs=1,verbose=2)
 
 test_generator.reset()
 pred=model.predict_generator(test_generator,
-steps=STEP_SIZE_TEST,
-verbose=1) 
+                            steps=STEP_SIZE_TEST,
+                            verbose=1)
 
 pred_bool = (pred >0.5)
  
