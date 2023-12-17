fcc4ec8cff412f8c7a13835db0a011bc0c90ff2a
ryalberti <ealberti@stevens.edu>
2023-12-16 21:24:23 -0500

    integrated license

--- geoguess.py
+++ geoguess.py
@@ -6,15 +6,19 @@
 import os 
 import argparse
 import aggregatecode as agc
-import language_detection
+import language_det as ladet
+from arch_classification.vgg_classify import arch_classify
+from License_Plate_Detection.license_det import license_det
 
 #####################################
 # define locations and var names 
 
 dir_path = os.getcwd()
-loc_path = dir_path + "\\locations"
-language_path = dir_path+"\\language_detection"
-json_path = language_path + "ee462finalproject-3e96cda81771.json"
+loc_path = dir_path + "\\locations\\"
+json_path = dir_path + "\\geoguesserautomation-5264cd87ce70.json"
+arch_path = dir_path + "\\arch_classification\\"
+mlb_path = arch_path + "mlb.pickle"
+arch_model = arch_path + "arch_model.pkl"
 
 
 #####################################
@@ -30,26 +34,27 @@ def parse_args():
     if not args.file:
         print("file input required.")
         quit()
-    return args
+    return args.file
 
 def process_license(input):
     # will call the necessary parts of the license plate functions 
     # returns list of license plates (or an empty list) 
-    ret = []
+    ret = license_det(input)
     print("License plate function")
+    print("License plate: " + ret)
     return ret
 
 def process_language(image_path):
-    print("Language detection",json_path)
-    detected_language = language_detection.detect_language(image_path)
+    print("Language detection")
+    detected_language = ladet.detect_language(image_path,json_path)
     print(f"Detected language: {detected_language}")
     ret = []
     return detected_language 
 
-def process_architecture(input):
+def process_architecture(image_path,model,labelbin):
     # will call architecture functions 
     # will return list of arch characteristics
-    ret = []
+    ret = arch_classify(image_path,model,labelbin)
     print("architecture")
     return ret 
 
@@ -65,12 +70,10 @@ def main():
 
     # TODO: fill these in as the models are completed. 
     license_output = process_license(input)
-    language_ouput = process_language(input)
-    arch_output = process_architecture(input)
+    language_ouput = process_language(input,json_path)
+    arch_output = process_architecture(input,arch_model,mlb_path)
 
     print(agc.process_tags(allLoc,(license_output,language_ouput,arch_output)))
 
 
-
-
-
+main()
